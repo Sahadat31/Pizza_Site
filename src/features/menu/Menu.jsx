1876e2c5bc9a1b3033db2719279a9b61090eda7d
akash3108 <prince31081999@gmail.com>
2024-09-18 20:29:02 +0530

    data loader react router

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+export default function Menu() {
+  const menu = useLoaderData()
+  console.log(menu)
+  return <ul>
+    {menu.map(pizza=><MenuItem key={pizza.id} pizza={pizza}/>)}
+  </ul>;
+}
+
+
+export const loader = async () => {
+  const menu = await getMenu()
+  return menu
+}
